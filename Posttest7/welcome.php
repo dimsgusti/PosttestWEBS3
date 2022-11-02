8df211dfa0b9e69390dab266ea32c3c4f0e3a772
dimsgusti <90297502+dimsgusti@users.noreply.github.com>
2022-11-02 22:50:55 +0800

    Add files via upload

---
+++ Posttest7/welcome.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="style.css">
+
+<div class="logout">
+
+    <marquee behavior="" direction=""><h3><a href="logout.php">Logout</a></h3></marquee>
+    <h3 style="color:white;">tangkap bang</h3>
+</div>
